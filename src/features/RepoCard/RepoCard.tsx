756ec3e9f135d29da8f6c6a0e101c228a681673a
rwithik <rwithik@gmail.com>
2024-04-24 10:14:18 +0530

    feat: add accessibility handlers

--- src/features/RepoCard/RepoCard.tsx
+++ src/features/RepoCard/RepoCard.tsx
@@ -7,35 +7,45 @@ export default function RepoCard() {
 
   if (!selectedItem) {
     return (
-      <div className="w-full p-4 rounded-md border border-slate-200 text-slate-400 shadow shadow-white">
+      <div className="w-3/4 p-4 rounded-md border border-slate-200 text-slate-400 shadow shadow-white">
         Select a repo to see its details
       </div>
     );
   }
 
   return (
-    <div className="w-full p-4 rounded-md border border-slate-200 shadow shadow-white flex flex-col gap-4">
-      <h2 className="text-lg font-semibold">{selectedItem?.name}</h2>
-      <div>{selectedItem?.description}</div>
-      <div className="flex gap-2 items-center text-sm">
-        <a className="cursor-pointer" href={selectedItem?.html_url}>
-          Visit the repo
-        </a>
-        <svg
-          onClick={() => copyText(selectedItem?.html_url)}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-4 h-4 cursor-pointer"
+    <div className="w-3/4 h-64 p-4 rounded-md border border-slate-200 shadow shadow-white flex gap-4">
+      <div className="flex flex-col basis-1/2">
+        <h2 className="text-lg font-semibold">{selectedItem?.name}</h2>
+        <div className="flex gap-2 items-center text-sm">
+          <a className="cursor-pointer" href={selectedItem?.html_url}>
+            Visit the repo
+          </a>
+          <svg
+            onClick={() => copyText(selectedItem?.html_url)}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-4 h-4 cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+            />
+          </svg>
+        </div>
+        <button
+          onClick={() => console.log("Deploy clicked")}
+          className="border self-start text-sm font-semibold border-white bg-white hover:bg-slate-100 text-slate-900 px-3 py-2 rounded-md mt-auto"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
-          />
-        </svg>
+          Deploy
+        </button>
+      </div>
+      <div className="basis-1/2 text-slate-400">
+        {selectedItem?.description}
       </div>
     </div>
   );
--- src/features/RepoSearch/RepoSearch.tsx
+++ src/features/RepoSearch/RepoSearch.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, KeyboardEvent, memo } from "react";
+import { ChangeEvent, KeyboardEvent, memo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useRepoSearchQuery } from "../../api";
 import { MAX_PAGE_NUMBER } from "../../config";
@@ -10,10 +10,17 @@ import {
 } from "./searchSlice";
 import { updateSelectedItem } from "../RepoCard/selectedRepoSlice";
 import { debounce } from "../../utils";
+import clsx from "clsx";
+
+const getOptionId = (id: number) => {
+  return `listbox-option-${id}`;
+};
 
 export default function RepoSearch() {
   const page = useSelector(selectPage);
   const searchQuery = useSelector(selectSearchQuery);
+  const [focusedOptionIndex, setFocusedOptionIndex] = useState<number>(-1);
+  const [isOpen, setIsOpen] = useState(false);
 
   const { data, isLoading, isSuccess, isError } = useRepoSearchQuery(
     { searchQuery, page, perPage: page < MAX_PAGE_NUMBER ? 8 : 4 },
@@ -23,30 +30,86 @@ export default function RepoSearch() {
 
   const handleSearchChange = debounce((e: ChangeEvent<HTMLInputElement>) => {
     dispatch(updateSearchQuery(e.target.value));
+    setIsOpen(true);
   });
 
   const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (!isSuccess) return false;
     switch (e.code) {
-      case "Down":
+      case "ArrowDown":
+        if (focusedOptionIndex === -1) {
+          setFocusedOptionIndex(0);
+          document
+            .getElementById(getOptionId(0))
+            ?.scrollIntoView({ behavior: "smooth" });
+        } else {
+          document
+            .getElementById(
+              getOptionId(
+                focusedOptionIndex === data.items.length - 1
+                  ? 0
+                  : focusedOptionIndex + 1
+              )
+            )
+            ?.scrollIntoView({ behavior: "smooth" });
+          setFocusedOptionIndex((prev) =>
+            prev === data.items.length - 1 ? 0 : prev + 1
+          );
+        }
+        break;
+      case "ArrowUp":
+        if (focusedOptionIndex === -1) {
+          setFocusedOptionIndex(data.items.length - 1);
+          document
+            .getElementById(getOptionId(data.items.length - 1))
+            ?.scrollIntoView({ behavior: "smooth" });
+        } else {
+          document
+            .getElementById(
+              getOptionId(
+                focusedOptionIndex === 0
+                  ? data.items.length - 1
+                  : focusedOptionIndex - 1
+              )
+            )
+            ?.scrollIntoView({ behavior: "smooth" });
+          setFocusedOptionIndex((prev) =>
+            prev === 0 ? data.items.length - 1 : prev - 1
+          );
+        }
+        break;
+      case "Esc":
+        setFocusedOptionIndex(-1);
+        setIsOpen(false);
+        break;
+      case "Enter":
+        dispatch(updateSelectedItem(data.items[focusedOptionIndex]));
+        break;
     }
   };
 
+  console.log(focusedOptionIndex);
+
   return (
     <>
-      {data?.items.length}
       <input
         className="h-8 text-sm w-full rounded-lg px-2"
         placeholder="Search GitHub Repositories"
         onChange={handleSearchChange}
+        onKeyDown={handleKeyDown}
       />
       {isLoading && "Loading..."}
       {isError && "Something went wrong"}
-      {isSuccess && (
+      {isSuccess && isOpen && (
         <div
           tabIndex={-1}
           className="w-full flex flex-col text-slate-100 border border-white shadow shadow-white rounded-md h-60 overflow-y-auto"
           role="listbox"
-          aria-activedescendant="listbox-option"
+          aria-activedescendant={
+            focusedOptionIndex !== -1
+              ? "listbox-option-" + focusedOptionIndex
+              : ""
+          }
           onScroll={(e) => {
             if (isLoading || page > MAX_PAGE_NUMBER) return;
             const target = e.target as HTMLDivElement;
@@ -56,15 +119,17 @@ export default function RepoSearch() {
               dispatch(updateToNextPage());
             }
           }}
-          onKeyDown={handleKeyDown}
         >
-          {data.items.map((i) => {
+          {data.items.map((i, index) => {
             return (
               <ListItem
+                isFocused={index === focusedOptionIndex}
                 key={i.id}
+                id={index}
                 onClick={() => dispatch(updateSelectedItem(i))}
                 name={i.name}
                 description={i.description}
+                onFocus={() => setFocusedOptionIndex(index)}
               />
             );
           })}
@@ -78,18 +143,28 @@ const ListItem = memo(
   ({
     onClick,
     name,
+    id,
     description,
+    onFocus,
+    isFocused,
   }: {
     onClick: () => void;
     name: string;
     description?: string;
+    onFocus: () => void;
+    id: number;
+    isFocused: boolean;
   }) => {
     return (
       <div
         onClick={onClick}
-        id="listbox-option"
+        id={getOptionId(id)}
         role="option"
-        className="hover:text-white hover:bg-slate-900 cursor-pointer px-4 py-2 first:rounded-t-md last:rounded-b-md flex gap-8 items-center justify-between"
+        className={clsx(
+          "hover:text-white hover:bg-slate-900 cursor-pointer px-4 py-2 first:rounded-t-md last:rounded-b-md flex gap-8 items-center justify-between",
+          isFocused && "text-white bg-slate-900"
+        )}
+        onMouseEnter={onFocus}
       >
         <span>{name}</span>
         {description ? (
