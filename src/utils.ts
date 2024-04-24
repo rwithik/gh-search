598ef3b04924a5c6355d2b356f7c460c79271644
rwithik <rwithik@gmail.com>
2024-04-24 11:13:20 +0530

    fix: fix type errors

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,10 @@
-export const debounce = <TFn extends (...args: any) => any>(
-  func: TFn,
+// any is fine here since we are handling it in the next line
+export const debounce = <TFn extends (...args: any) => void>(
+  func: TFn extends (...args: infer R) => void ? (...args: R) => void : never,
   timeout: number = 500
 ) => {
   let timer: ReturnType<typeof setTimeout> | null;
-  return function (this: any, ...args: Parameters<TFn>) {
+  return function (this: ThisType<TFn>, ...args: Parameters<TFn>) {
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
       timer = null;
